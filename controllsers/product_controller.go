5944a8ea5b41188d6af11b88e4bd3ecb8b877287
Dstar18 <dedybintang18@gmail.com>
2025-03-03 20:52:43 +0700

    Fix: product delete

--- controllsers/product_controller.go
+++ controllsers/product_controller.go
@@ -90,3 +90,92 @@ func ProductStore(c echo.Context) error {
 		"message": "Created successfully",
 	})
 }
+
+func ProductUpdate(c echo.Context) error {
+	// request param id
+	id := c.Param("id")
+
+	// request struct model
+	var productM models.Product
+
+	// check data by id
+	if err := config.DB.First(&productM, id).Error; err != nil {
+		utils.Logger.Warn("Data Not found")
+		return c.JSON(http.StatusNotFound, map[string]interface{}{
+			"code":    http.StatusNotFound,
+			"message": "Data Not Found",
+		})
+	}
+
+	// request struct validation
+	var product ProductValidate
+
+	// request params, and check body
+	if err := c.Bind(&product); err != nil {
+		utils.Logger.Error("Invalid request body")
+		return c.JSON(http.StatusBadRequest, map[string]interface{}{
+			"code":    http.StatusBadRequest,
+			"message": "invalid request body",
+		})
+	}
+
+	// validation struc
+	validate := validator.New()
+	if err := validate.Struct(product); err != nil {
+		errors := make(map[string]string)
+		for _, err := range err.(validator.ValidationErrors) {
+			errors[err.Field()] = "This field is" + " " + err.Tag() + " " + err.Param()
+		}
+		utils.Logger.Error(errors)
+		return c.JSON(http.StatusBadRequest, map[string]interface{}{
+			"code":    http.StatusBadRequest,
+			"message": errors,
+		})
+	}
+
+	// save to db
+	productM.Name = product.Name
+	productM.Desciprtion = product.Desciprtion
+	productM.Price = product.Price
+
+	if err := config.DB.Save(&productM).Error; err != nil {
+		utils.Logger.Error(err.Error())
+		return c.JSON(http.StatusInternalServerError, map[string]interface{}{
+			"code":    http.StatusInternalServerError,
+			"message": err.Error(),
+		})
+	}
+
+	// return success
+	utils.Logger.Info("Update successfully")
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"code":    http.StatusOK,
+		"message": "Update successfully",
+		"data":    productM,
+	})
+}
+
+func ProductDestroy(c echo.Context) error {
+	// request param id
+	id := c.Param("id")
+
+	// request struct model
+	var productM models.Product
+
+	// check data by id
+	if err := config.DB.First(&productM, id).Error; err != nil {
+		utils.Logger.Warn("Data Not found")
+		return c.JSON(http.StatusNotFound, map[string]interface{}{
+			"code":    http.StatusNotFound,
+			"message": "Data Not Found",
+		})
+	}
+
+	// return success
+	utils.Logger.Info("Deleted successfully")
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"code":    http.StatusOK,
+		"message": "Deleted successfully",
+		"data":    productM,
+	})
+}
--- main.go
+++ main.go
@@ -47,6 +47,7 @@ func main() {
 
 	protected.GET("/product", controllsers.Products)
 	protected.POST("/product/add", controllsers.ProductStore)
+	protected.POST("/product/update/:id", controllsers.ProductUpdate)
 
 	e.Logger.Fatal(e.Start(":3000"))
 }
