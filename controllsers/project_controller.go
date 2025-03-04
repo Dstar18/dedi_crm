7e261d1db6f61993d592cb1ae2366527768984e8
Dstar18 <dedybintang18@gmail.com>
2025-03-04 08:06:46 +0700

    Fix: project add

--- controllsers/project_controller.go
+++ controllsers/project_controller.go
@@ -1,18 +1,74 @@
 package controllsers
 
 import (
+	"dedi_crm/config"
+	"dedi_crm/models"
 	"dedi_crm/utils"
 	"net/http"
+	"time"
 
+	"github.com/go-playground/validator/v10"
 	"github.com/labstack/echo/v4"
 )
 
+type StoreValidate struct {
+	LeadID    uint   `json:"lead_id" validate:"required"`
+	ProductID string `json:"product_id" validate:"required"`
+}
+
 func ProjectAdd(c echo.Context) error {
+	// request struct validation
+	var project StoreValidate
+
+	// request params, and check body
+	if err := c.Bind(&project); err != nil {
+		utils.Logger.Error("Invalid request body")
+		return c.JSON(http.StatusBadRequest, map[string]interface{}{
+			"code":    http.StatusBadRequest,
+			"message": "invalid request body",
+		})
+	}
+
+	// validation struc
+	validate := validator.New()
+	if err := validate.Struct(project); err != nil {
+		errors := make(map[string]string)
+		for _, err := range err.(validator.ValidationErrors) {
+			errors[err.Field()] = "This field is" + " " + err.Tag() + " " + err.Param()
+		}
+		utils.Logger.Error(errors)
+		return c.JSON(http.StatusBadRequest, map[string]interface{}{
+			"code":    http.StatusBadRequest,
+			"message": errors,
+		})
+	}
+
+	// chek islogin
+	emailIsLogin := c.Get("email").(string)
+	var userM models.User
+	config.DB.Where("email = ? ", emailIsLogin).First(&userM)
+
+	param := models.Project{
+		LeadID:     project.LeadID,
+		ProductID:  project.ProductID,
+		Status:     "pending",
+		CreatedAt:  time.Now().Format("2006-01-02 15:04:05"),
+		AssignedTo: userM.ID,
+	}
+
+	// create to db
+	if err := config.DB.Create(&param).Error; err != nil {
+		utils.Logger.Error(err.Error())
+		return c.JSON(http.StatusInternalServerError, map[string]interface{}{
+			"code":    http.StatusInternalServerError,
+			"message": err.Error(),
+		})
+	}
+
 	// return success
 	utils.Logger.Info("Project Created successfully")
 	return c.JSON(http.StatusOK, map[string]interface{}{
 		"code":    http.StatusOK,
 		"message": "Project Created successfully",
-		"Data":    nil,
 	})
 }
--- models/project.go
+++ models/project.go
@@ -3,9 +3,10 @@ package models
 type Project struct {
 	ID         uint   `gorm:"primaryKey" json:"id"`
 	LeadID     uint   `gorm:"not null" json:"lead_id"`                      //FK leadID
+	AssignedTo uint   `gorm:"not null" json:"assign_to"`                    //FK userID (sales)
 	ProductID  string `gorm:"type:varchar(100);not null" json:"product_id"` //Array [1,3,4]
 	Status     string `gorm:"type:varchar(50);not null;default:'pending';check:status IN ('pending', 'approved', 'rejected')" json:"status"`
-	ApprovalBy uint   `gorm:"not null" json:"approval_by"` //FK userID (manager)
+	ApprovalBy uint   `gorm:"default:null" json:"approval_by"` //FK userID (manager)
 	ApprovedAt string `gorm:"type:timestamptz;default:null" json:"approved_at"`
 	CreatedAt  string `gorm:"type:timestamptz;default:null" json:"created_at"`
 	UpdatedAt  string `gorm:"type:timestamptz;default:null" json:"updated_at"`
